bfc86e684d63b225cc8fa3ff8767d105933387a9
FlameOnOn <31836982+FlameOnOn@users.noreply.github.com>
2018-11-09 11:24:04 +0800

    Update TopologicalSort.java

--- TopologicalSort.java
+++ TopologicalSort.java
@@ -1,9 +1,14 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TopologicalSort {
 	public static void main(String args[]){
 		
 		int n = 12 ;
 		int inDgree[] = new int[13] ;
+		int topoSortResult[] = new int[13] ;
+		int current = 0 ;
 		int figure[][] = {{0,1,0,0,0,1,1,0,0,0,0,0,0},
 				          {0,0,0,0,0,0,0,0,0,0,0,0,0},
 				          {1,0,0,1,0,0,0,0,0,0,0,0,0},
@@ -17,7 +22,44 @@ public class TopologicalSort {
 				          {0,0,0,0,0,0,0,0,0,0,0,0,0},
 				          {0,0,0,0,0,0,0,0,0,0,0,0,1},
 				          {0,0,0,0,0,0,0,0,0,0,0,0,0}} ;
-		//https://www.cnblogs.com/lemon-flm/p/7877898.html
+		int j = 0 ;
+		int sum = 0 ;
+		while(j < 13){
+			for (int i = 0 ; i < 13 ; i++){
+				sum += figure[i][j] ;
+			}
+			inDgree[j] = sum ;
+			j++ ;
+			sum = 0 ;
+		}
+		
+		Queue<Integer> queue = new LinkedList<Integer>() ;
+		for(int i  = 0 ; i < 13 ; i++){
+			if (inDgree[i] == 0)
+				queue.offer(i) ;
+		}
+		
+		/*for (int i : queue){
+			System.out.print(i);
+		}*/
+	
+		while(!queue.isEmpty()){
+			int curr = queue.poll() ;
+			topoSortResult[current++] = curr ;
+			for (int i = 0 ; i < 13 ; i++){
+				if(figure[curr][i] != 0){
+					inDgree[i]-- ;
+					if(inDgree[i] == 0)
+						queue.offer(i) ;
+				}
+					
+			}
+			
+		}
+		
+		for (int i : topoSortResult){
+			System.out.print(i + " ") ;
+		}
 		
 	}
 }

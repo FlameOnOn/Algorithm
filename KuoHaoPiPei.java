7f9a8886e5098156d9060ef1d564dbf1374003fd
FlameOnOn <31836982+FlameOnOn@users.noreply.github.com>
2018-11-12 10:33:35 +0800

    Create KuoHaoPiPei.java

---
+++ KuoHaoPiPei.java
@@ -0,0 +1,32 @@
+public class KuoHaoPiPei {
+	public static void main(String args[]){
+		
+		String s = "({][(){)}{(" ;
+		String s2 = "()()(({{}})){}([[[]]])" ;
+		System.out.println(isMatch(s)) ;
+		System.out.println(isMatch(s2)) ;
+		
+	}
+
+	private static boolean isMatch(String s) {
+		Stack<Character> stack = new Stack<Character>() ;
+		for (int i = 0 ; i < s.length() ; i ++){
+			if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+				stack.push(s.charAt(i));
+			}else{
+				if(stack.isEmpty()){
+					return false;
+				}else{
+					char firstElement = stack.pop() ;
+					if ((s.charAt(i) == ')' && firstElement == '(') || 
+							(s.charAt(i) == ']' && firstElement == '[') ||
+							(s.charAt(i) == '}' && firstElement == '{')){
+						continue ;
+					}
+				}
+			}
+		}
+		
+		return stack.isEmpty() ;
+	}
+}

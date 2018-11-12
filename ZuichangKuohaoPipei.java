be72613b2b68d7ac2fbbfa6415d73fffa80c4f8e
FlameOnOn <31836982+FlameOnOn@users.noreply.github.com>
2018-11-12 13:56:46 +0800

    Update ZuichangKuohaoPipei.java

--- ZuichangKuohaoPipei.java
+++ ZuichangKuohaoPipei.java
@@ -1,10 +1,47 @@
-// this has a O(1) spatial complexity sulution, to use an int value instead of a stack to realize it.
 public class ZuichangKuohaoPipei {
 	public static void main(String args[]){
 		String s = "((()))()())(" ;
+		String s2 = "(((()()))" ;
+		String s3 = "()()()()" ;
+		
 		System.out.println(longestMatchedKuohao(s));
+		System.out.println(longestMatchedKuohaoWithoutStack(s)) ;
+		
+		System.out.println(longestMatchedKuohao(s2));
+		System.out.println(longestMatchedKuohaoWithoutStack(s2)) ;
+		
+		System.out.println(longestMatchedKuohao(s3));
+		System.out.println(longestMatchedKuohaoWithoutStack(s3)) ;
 	}
 
+	// using stack, the spatial complexity is O(1)
+	private static int longestMatchedKuohaoWithoutStack(String s) {
+		int start = -1 ;
+		int result = 0 ;
+		int deep = 0 ;
+		for (int i = 0 ; i < s.length() ; i++){
+			if (s.charAt(i) == '('){
+				deep++ ;
+			}else{
+				if(deep == 0){
+					start = i ;
+					continue ;
+				}else{
+					deep-- ;
+					if(deep == 0){
+						result = Math.max(result, i - start) ;
+					}else{
+						result = Math.max(result, i - deep+1) ;
+					}
+				}
+			}
+		}
+		
+		
+		return result ;
+	}
+
+	// using stack, the spatial complexity is O(N)
 	private static int longestMatchedKuohao(String s) {
 		int start = -1 ;
 		int result = 0 ;

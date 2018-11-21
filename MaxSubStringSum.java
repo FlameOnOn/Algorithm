d3891886ebb50bd37808d5e5cdf9a8e64ec2e0dd
FlameOnOn <31836982+FlameOnOn@users.noreply.github.com>
2018-11-21 17:45:29 +0800

    Update MaxSubStringSum.java

--- MaxSubStringSum.java
+++ MaxSubStringSum.java
@@ -12,17 +12,24 @@ public class MaxSubStringSum {
 		int sum[] = new int[text.length] ;
 		int result = -1 ;
 		sum[0] = text[0] ;
+		int from = 0 , to = 0 ;
 		for(int i = 1 ; i < text.length ; i++){
 			if(sum[i-1]  > 0){
 				sum[i] = sum[i-1] + text[i] ;
 			}else{
 				sum[i] = text[i] ;
+				from = i ;
 			}
 			if (sum[i] > result){
 				result = sum[i] ;
+				to = i ;
 			}
 		}
-		
+		StringBuilder sb = new StringBuilder() ;
+		for(int i = from ; i <= to ; i++){
+			sb.append(text[i] + " ") ;
+		}
+		System.out.println("The max substring is " + sb.toString()) ;
 		return result ;
 	}
 

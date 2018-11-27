f976a89b7154fe89103915a75b5edd87e84e7790
FlameOnOn <31836982+FlameOnOn@users.noreply.github.com>
2018-11-27 17:09:40 +0800

    Create GedianGebian.java

---
+++ GedianGebian.java
@@ -0,0 +1,59 @@
+
+public class GedianGebian {
+	public static void main(String args[]){
+		int graph[][] = new int[13][13] ;
+		
+		graph[0][1] = 1 ;
+		graph[0][2] = 1 ;
+		graph[0][12] = 1 ;
+		graph[0][11] = 1 ;
+		
+		graph[1][0] = 1 ;
+		graph[1][2] = 1 ;
+		graph[1][5] = 1 ;
+		graph[1][6] = 1 ;
+		graph[1][9] = 1 ;
+		graph[1][3] = 1 ;
+		graph[1][4] = 1 ;
+		
+		graph[2][0] = 1 ;
+		graph[2][1] = 1 ;
+		
+		graph[3][1] = 1 ;
+		graph[3][4] = 1 ;
+		
+		graph[4][1] = 1 ;
+		graph[4][3] = 1 ;
+		
+		graph[5][1] = 1 ;
+		graph[5][6] = 1 ;
+		graph[5][7] = 1 ;
+		graph[5][8] = 1 ;
+		
+		graph[6][1] = 1 ;
+		graph[6][5] = 1 ;
+		graph[6][7] = 1 ;
+		
+		graph[7][5] = 1 ;
+		graph[7][6] = 1 ;
+		
+		graph[8][5] = 1 ;
+		
+		graph[9][1] = 1 ;
+		graph[9][10] = 1 ;
+		graph[9][11] = 1 ;
+		
+		graph[10][11] = 1 ;
+		graph[10][9] = 1 ;
+		
+		graph[11][0] = 1 ;
+		graph[11][9] = 1 ;
+		graph[11][10] = 1 ;
+		
+		graph[12][0] = 1 ;
+		
+		
+		
+		
+	}
+}
